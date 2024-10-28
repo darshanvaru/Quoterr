14fcc187e7c66f4da5b511f90feea31b25acae05
Darshan Varu <darshanvaru2003@gmail.com>
2024-10-28 17:30:00 +0530

    Removed all the debug statements

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -10,24 +10,11 @@ class ApiService {
   Future fetchRandomQuote(String category) async {
     final url = Uri.parse('https://api.api-ninjas.com/v1/quotes?category=$category');
 
-    print("----------------------------------------------------");
-    print(url);
-    print("----------------------------------------------------");
-
     final response = await http.get(url, headers: {
       'X-Api-Key': apiKey,
     });
 
-    print("----------------------------------------------------");
-    print("Reponce sent");
-    print(response);
-    print("----------------------------------------------------");
-
     if (response.statusCode == 200) {
-      print("----------------------------------------------------");
-      print("Response status: 200 OK");
-      print("Response body: ${response.body}");
-      print("----------------------------------------------------");
 
       List data = json.decode(response.body);
       if (data.isNotEmpty) {
@@ -36,10 +23,6 @@ class ApiService {
         throw Exception('No quotes found for this category.');
       }
     } else {
-      print("----------------------------------------------------");
-      print("Failed to fetch quote. Status code: ${response.statusCode}");
-      print("Response body: ${response.body}");
-      print("----------------------------------------------------");
       throw Exception('Failed to load quotes');
     }
   }
